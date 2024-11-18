c851b1f14d508d544e7e82ba14efaca6d83a9693
MirejeteKastratii <mirejetak@gmail.com>
2024-11-18 18:41:34 +0100

    punova

---
+++ hyrjenejs/script.js
@@ -0,0 +1,21 @@
+// deklarimi i variables = krijojme nje variabel
+let testVar;
+// inicializim - variables i japim vlere per here te pare
+testVar = "m";
+
+// deklarimi dhe inicializimi ne te njejtin rresht
+const PI = 3.14; //constanta eshte vlere e pandryshueshme
+
+// variablat & datatypes:
+let emri = "mirjeta"; //string
+let notaMatematike = 1; //number
+let isAdult = false; //true //boolean
+let x; //undefined
+let nullVariabla = null;
+
+// case sensitive:
+let Emri = "Lira";
+
+// alert("hello");
+// confirm("close window?");
+prompt("shkruaj emrin");
